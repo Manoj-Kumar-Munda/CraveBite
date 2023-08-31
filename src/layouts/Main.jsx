5800433b86e33daada4c5d63cc89a64b75105f1d
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2023-08-31 19:57:14 +0530

    vegOnly filter added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import Nav from '../components/Nav'
-import Footer from '../components/Footer'
+
 import { Outlet } from 'react-router-dom'
 import Header from '../components/Header'
 import useOnlineStatus from '../utils/useOnlineStatus'
@@ -21,7 +20,7 @@ const Main = () => {
   return (
     <div className=''>
         <Header />
-        <main className='min-h-screen'>
+        <main className=''>
             <Outlet />
         </main>
     </div>
